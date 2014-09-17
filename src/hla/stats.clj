19f9c607dc3df370f3c61164c26d38d01bf8f060
theoretick <me@lucascharles.me>
2014-09-17 08:14:22 -0700

    expanded stats ns with helper functions

--- src/hla/stats.clj
+++ src/hla/stats.clj
@@ -1,7 +1,30 @@
-(ns hla.stats)
+(ns hla.stats
+  (:require [clojure.math.numeric-tower :as math]))
+
+(defn sum
+  [data]
+  (reduce + data))
+
+(defn mean
+  [data]
+  (/ (sum data) (count data))
+
+(defn absolute-deviations
+  "Set of absolute deviations for given dataset"
+  [data]
+  (let [m (mean data)]
+    (map (fn [x] (math/abs (- x m)))
+      data)))
+
+(defn max-absolute-deviation
+  "Maximum absolute deviation from a given dataset"
+  [data]
+  (apply max (absolute-deviations data)))
 
 (defn pop-var
   "Population Variance of given dataset"
   [data]
-  42
-  )
+  (let [m (mean data)]
+    (sum (reduce (fn [x] (math/expt (- x m) 2)) data))
+)))
+
